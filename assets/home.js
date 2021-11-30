7aa67f1993b813f49d3efac14ba71fb38a136c67
baynv910 <bay.nv910@gmail.com>
2021-12-01 03:30:30 +0700

    new ver

---
+++ assets/home.js
@@ -0,0 +1,36 @@
+
+// Set the date we're counting down to
+var countingDate = new Date("Feb 14, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = now - countingDate;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.querySelector("#count span._day").innerHTML =  (days < 10? "0": "") + days;
+  document.querySelector("#count span._hour").innerHTML = (hours < 10? "0": "") + hours;
+  document.querySelector("#count span._minute").innerHTML = (minutes < 10? "0": "") + minutes;
+  document.querySelector("#count span._second").innerHTML = (seconds < 10? "0": "") + seconds;
+}, 1000);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop < 250 || document.documentElement.scrollTop < 250) {
+    document.querySelector('#text').style.opacity = (300 - document.documentElement.scrollTop)/100;
+  } else {
+    document.querySelector('#text').style.opacity = 0;
+  }
+}
